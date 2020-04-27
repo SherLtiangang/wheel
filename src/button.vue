ca8b939c839ea2a517825fbd64ff9eecb92d00fe
holmes <15221038479@163.com>
2020-04-27 21:48:12 +0800

    update

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,7 @@
     export default {
         // props: ['icon','iconPosition'],
         props: {
-            icon: {},
+            icon: {}, 
             iconPosition: {
                 type: String,
                 default: 'left',
